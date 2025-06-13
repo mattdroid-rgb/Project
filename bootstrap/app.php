1be40373a10e283f6792329acfa306338443c7b0
Matt Pino <mattpino678@gmail.com>
2025-06-13 21:26:23 +0800

    Act 5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use IlluminateFoundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
