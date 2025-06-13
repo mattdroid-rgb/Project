327a74f6ba96370fa2fd26c6160bf77f288ff2fb
Matt Pino <mattpino678@gmail.com>
2025-06-13 21:48:16 +0800

    Act 5

--- app/Http/Middleware/NoCache.php
+++ app/Http/Middleware/NoCache.php
@@ -12,8 +12,5 @@ class NoCache
     {
         $response = $next($request);
 
-        return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-                        ->header('Pragma', 'no-cache')
-                        ->header('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
-    }
+        
 }
