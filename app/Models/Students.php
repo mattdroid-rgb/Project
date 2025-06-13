d4ea52bd2549792689aeafd4462a7cad98a20bc8
Matt Pino <mattpino678@gmail.com>
2025-06-13 21:50:30 +0800

    Act 3

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -7,11 +7,6 @@ use Illuminate\Notifications\Notifiable;
 
 class Students extends Model
 {
-    use Notifiable;
-
-    protected $table = 'students';
-    protected $fillable = [
-        'name',
-        'age',
+   
     ];
 }
